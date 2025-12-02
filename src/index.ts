8a03620d9a82710cf50b83ad950de39663d01f80
Jack Steam <jsteam@emergence.ai>
2025-12-01 18:24:15 -0600

    security: remove self-update mechanism for security

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { Command } from 'commander';
 import { createAuthCommand } from './commands/auth.ts';
 import { createConversationsCommand } from './commands/conversations.ts';
 import { createMessagesCommand } from './commands/messages.ts';
-import { createUpdateCommand } from './commands/update.ts';
-import { checkForUpdates } from './lib/updater.ts';
 import chalk from 'chalk';
 
 const program = new Command();
@@ -19,12 +17,6 @@ program
 program.addCommand(createAuthCommand());
 program.addCommand(createConversationsCommand());
 program.addCommand(createMessagesCommand());
-program.addCommand(createUpdateCommand());
-
-// Check for updates asynchronously (non-blocking)
-checkForUpdates(true).catch(() => {
-  // Silently fail update check
-});
 
 // Parse arguments
 program.parse(process.argv);
